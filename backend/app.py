c9b299b272c0f93ead593341f546c0f20dc58f6a
Anupam Dwivedi <dwivedianupam694@gmail.com>
2026-05-17 02:15:50 +0530

    fix render deployment

--- backend/app.py
+++ backend/app.py
@@ -141,9 +141,10 @@ def verify_vote(voter_id):
         "has_voted": voted,
         "registered": voter_exists(voter_id)
     })
-
+import os
 
 if __name__ == "__main__":
-    import os
-
-app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+       host="0.0.0.0", 
+       port=int(os.environ.get("PORT", 5000))
+    )
