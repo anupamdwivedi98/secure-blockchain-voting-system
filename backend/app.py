928852336659c4e58c1754eac8d367f30df18f9f
Anupam Dwivedi <dwivedianupam694@gmail.com>
2026-05-17 02:12:51 +0530

    fix render port

--- backend/app.py
+++ backend/app.py
@@ -144,4 +144,6 @@ def verify_vote(voter_id):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
